79562f45a11a9da06bc5cddc182c45427c05f93a
Alisher <alisher@prodatakey.com>
2025-05-15 19:20:33 +0500

    upgrades deps version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,14 +41,14 @@ ksp {
 
 dependencies {
     implementation("androidx.room:room-runtime:2.7.1")
-    ksp("androidx.room:room-compiler:2.7.0")
+    ksp("androidx.room:room-compiler:2.7.1")
 
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
 
     testImplementation("junit:junit:4.13.2")
